acf4812f000a601c32d9d9952c43157208c5e412
Joao Martins <milanezijoao51@gmail.com>
2024-01-09 12:32:07 -0300

    Criando a interface IRepository e implementando ela na classe Repository

---
+++ Finance management/Controllers/PersonController.cs
@@ -0,0 +1,15 @@
+﻿using Finance_management.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finance_management.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class PersonController:ControllerBase
+{
+    private readonly AppDBContext _Context;
+
+    public PersonController(AppDBContext context)
+    {
+       _Context = context;
+    }
+}
---
+++ Finance management/Repository/Repository.cs
@@ -0,0 +1,45 @@
+﻿
+using Finance_management.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance_management.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity>where TEntity : class
+    {
+        private readonly AppDBContext _context;
+
+        public Repository(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<TEntity>().FindAsync(id);
+            _context.Set<TEntity>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
